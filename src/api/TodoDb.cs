08b358e0afcbdc2e410a5296f686eedffcc63b7b
Thiago Lunardi <thl@templafy.com>
2024-07-13 12:26:32 +0200

    chore: Add method to safely insert todo item into database

--- src/api/TodoDb.cs
+++ src/api/TodoDb.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using SimpleTodo.Api;
 
 public class TodoDb : DbContext
 {
-    private static string ConnectionString = "Data Source=todos.db;Password=Password123";
+    private string? _connectionString;
+
+    public TodoDb(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void AddTodoItemUnsafe(string todoItemTitle)
+    {
+        var query = $"INSERT INTO TodoItems (Title) VALUES ('{todoItemTitle}');"; // Vulnerable to SQL Injection
+
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            var command = new SqlCommand(query, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+    }
+    
     public TodoDb(DbContextOptions options) : base(options) { }
     public DbSet<TodoItem> Items => Set<TodoItem>();
     public DbSet<TodoList> Lists => Set<TodoList>();
