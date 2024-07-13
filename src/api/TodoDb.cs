f9516500eff4fb0ea29220d901dcd705092826af
Thiago Lunardi <thl@templafy.com>
2024-07-13 12:21:15 +0200

    chore: Update TodoDb connection string

--- src/api/TodoDb.cs
+++ src/api/TodoDb.cs
@@ -3,6 +3,7 @@ using SimpleTodo.Api;
 
 public class TodoDb : DbContext
 {
+    private static string ConnectionString = "Data Source=todos.db;Password=Password123";
     public TodoDb(DbContextOptions options) : base(options) { }
     public DbSet<TodoItem> Items => Set<TodoItem>();
     public DbSet<TodoList> Lists => Set<TodoList>();
