2f20d6f0a27e607b6ff0490b52c12e1b6cd235c3
Thiago Lunardi <thl@templafy.com>
2024-07-14 13:26:53 +0200

    chore: Add DbConsole project and update solution file

---
+++ src/DbConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DbConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "'; DROP TABLE Users; --";
+            string connectionString = "your_connection_string_here";
+            string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}", reader[0]));
+                }
+            }
+        }
+    }
+}
